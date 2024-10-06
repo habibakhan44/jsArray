f9ee20aa257c143b89073722ee56a993c8516da1
habibakhan44 <152440245+habibakhan44@users.noreply.github.com>
2024-10-06 12:05:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+// let x= 5;
+// function sayHello(){
+    
+//         var x = 2;
+   
+//         console.log(x);
+      
+        
+// }
+
+// console.log(x);
+// sayHello();
+
+
+// function sayHello(num1 , num2){
+//     console.log(num1 + num2);
+
+// }
+// sayHello(2,6);
+
+// let dayOfWk = prompt("Enter Day of Week");
+// let lower = dayOfWk.toLowerCase(); 
+
+// switch (lower) { 
+//     case "sat":
+//     case "sun":
+//         alert("Whoopee");
+//         break;
+//     case "fri":
+//         alert("TGIF!");
+//         break;
+//     default:
+//         alert("Shoot me now!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
